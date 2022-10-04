6ce9f94db8a2b936c9378b013ce323f9604abe31
Sony Huynh <sony.huynh@employmenthero.com>
2022-10-04 16:01:28 +0700

    #init deploy-action

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM ehdevops/action-deploy:1.0.0
+FROM ehdevops/action-deploy:0.0.1
 COPY entrypoint.sh /entrypoint.sh
-ENTRYPOINT ["sh", "/entrypoint.sh"]
+ENTRYPOINT ["bash", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 usage_docs() {
   echo ""
@@ -10,22 +10,162 @@ usage_docs() {
   echo "    image_name: ehdevops/example:1.0.0"
   echo "    cluster_context: staging"
   echo "    author: tbd"
-  echo "    check: false"
+  echo "    trigger_deploy: true"
+  echo "    wait_deploy: false"
+  echo "    deploy_token: \${{ github.token }}"
+  echo "    wait_interval: 30"
+
+}
+API_STG="hero2.staging.ehrocks.com:443"
+API_INT="hero2.integration.ehrocks.com:443"
+API_PROD="hero2.ehrocks.com:443"
+
+_validate_args() {
+
+  if [ -z "${INPUT_APP_NAME}" ]
+  then
+    echo "Error: app_name is a required argument."
+    usage_docs
+    exit 1
+  fi
+
+  if [ -z "${INPUT_IMAGE_NAME}" ]
+  then
+    echo "Error: image_name is a required argument."
+    usage_docs
+    exit 1
+  fi
+
+  if [ -z "${INPUT_CLUSTER_CONTEXT}" ]
+  then
+    echo "Error: cluster_context is required"
+    usage_docs
+    exit 1
+  fi
+
+  if [ -z "${INPUT_AUTHOR}" ]
+  then
+    echo "Error: author is required"
+    usage_docs
+    exit 1
+  fi
+
+  if [ -z "${INPUT_TRIGGER_DEPLOY}" ]
+  then
+    echo "Error: trigger_deploy is required"
+    usage_docs
+    exit 1
+  fi
+
+  if [ -z "${INPUT_TRIGGER_DEPLOY}" ]
+  then
+    echo "Error: trigger_deploy is required"
+    usage_docs
+    exit 1
+  fi
+
+  if [ -z "${INPUT_WAIT_DEPLOY}" ]
+  then
+    echo "Error: wait_deploy is required"
+    usage_docs
+    exit 1
+  fi
+
+  if [ -z "${INPUT_DEPLOY_TOKEN}" ]
+  then
+    echo "Error: deploy_token is required"
+    usage_docs
+    exit 1
+  fi
+
+  if [ -z "${INPUT_WAIT_INTERVAL}" ]
+  then
+    INPUT_WAIT_INTERVAL=30
+  fi
+
 }
 
-validate_args() {
-  app_name=employment-hero
-  if [ "${INPUT_APP_NAME}" ]
+_lets_wait() {
+  echo "Sleeping for ${INPUT_WAIT_INTERVAL} seconds"
+  sleep $INPUT_WAIT_INTERVAL
+}
+
+_gen_token() {
+  if [ "${INPUT_CLUSTER_CONTEXT}" = "staging" ]
   then
-    range=${INPUT_APP_NAME}
+    eval "$(herocli --server ${API_STG} login ${INPUT_DEPLOY_TOKEN} | tail -1 | xargs )"
+  elif [ "${INPUT_CLUSTER_CONTEXT}" = "intergration" ]
+  then
+    eval "$(herocli --server ${API_INT} login ${INPUT_DEPLOY_TOKEN} | tail -1 | xargs )"
+  elif [ "${INPUT_CLUSTER_CONTEXT}" = "production" ]
+  then
+    eval "$(herocli --server ${API_PROD} login ${INPUT_DEPLOY_TOKEN} | tail -1 | xargs )"
   fi
 }
 
 
+_deploy() {
+  if [ "${INPUT_CLUSTER_CONTEXT}" = "staging" ]
+  then
+    herocli --server ${API_STG} app deploy ${INPUT_APP_NAME} --image ${INPUT_IMAGE_NAME} --user ${INPUT_AUTHOR}
+  elif [ "${INPUT_CLUSTER_CONTEXT}" = "intergration" ]
+  then
+    herocli --server ${API_INT} app deploy ${INPUT_APP_NAME} --image ${INPUT_IMAGE_NAME} --user ${INPUT_AUTHOR}
+  elif [ "${INPUT_CLUSTER_CONTEXT}" = "production" ]
+  then
+    herocli --server ${API_PROD} app deploy ${INPUT_APP_NAME} --image ${INPUT_IMAGE_NAME} --user ${INPUT_AUTHOR}
+  fi  
+}
+
+
+_get_deployment_status() {
+  server=$1
+  herocli --server ${server} app deployment-status ${INPUT_APP_NAME} | grep "Deployment status" | awk '{print $3}'
+}
+
+_wait_for_deploy_to_finsh() {
+  STATUS="NULL"
+  if [ "${INPUT_CLUSTER_CONTEXT}" = "staging" ]
+  then
+    while ! [[ "${STATUS}" =~ "Success" ]]
+    do
+      _lets_wait
+      STATUS=$(_get_deployment_status "${API_STG}")
+    done
+  elif [ "${INPUT_CLUSTER_CONTEXT}" = "intergration" ]
+  then
+    while ! [[ "${STATUS}" =~ "Success" ]]
+    do
+      _lets_wait
+      STATUS=$(_get_deployment_status "${API_INT}")
+    done
+  elif [ "${INPUT_CLUSTER_CONTEXT}" = "production" ]
+  then
+    while ! [[ "${STATUS}" =~ "Success" ]]
+    do
+      _lets_wait
+      STATUS=$(_get_deployment_status "${API_PROD}")
+    done
+  fi  
+}
+
 main() {
-  validate_args
+  _validate_args
+  _gen_token
 
+  if [ "${INPUT_TRIGGER_DEPLOY}" = true ]
+  then
+    _deploy
+  else
+    echo "Skipping triggering the deployment."
+  fi
 
+  if [ "${INPUT_WAIT_DEPLOY}" = true ]
+  then
+      _wait_for_deploy_to_finsh
+  else
+    echo "Skipping waiting for deployment."
+  fi
 }
 
 main
